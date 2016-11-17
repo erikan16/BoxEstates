2fb47af4040773b1cfffbbc54504492ac4318d62
erikan16 <erikan16@gmail.com>
2016-11-16 21:49:33 -0500

    buy page

--- resources/views/pages/buy.blade.php
+++ resources/views/pages/buy.blade.php
@@ -36,56 +36,51 @@
 
 @section('content')
     <!-- Main Page -->
-    <div class="ui middle aligned stackable grid container">
+    <div class="ui middle aligned grid container">
         <div class="row articleHeader">
             <div class="nine wide column">
                 <h1>Buying</h1>
             </div>
         </div>
-        <div class="row mapContainer">
-            <div class="ui grid internally celled">
-                <div class="ui equal width stackable internally celled grid">
-                    <div class="sixteen wide column">
 
-                        <div class="ui three column grid">
-                            @foreach($properties as $property)
-                                {{--<p>{{ print_r($property->getFirstImage()) }}</p>--}}
-                            <div class="column">
-                                <div class="ui special card">
-                                    <div class="blurring dimmable image">
-                                        <div class="ui dimmer">
-                                            <div class="content">
-                                                <div class="center">
-                                                    <h2 class="ui price header">${{ $property->price }}</h2>
-                                                    <a href="{{ route('property.single', $property->id) }}">
-                                                        <div class="ui inverted button">Show Property</div>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <img src="{{ asset($property->getFirstImage()) }}" style="height: 250px">
-                                        {{--<img src="/images/property/{{ $property->getFirstImage()->file_name  }}">--}}
-                                    </div>
+        <div class="sixteen wide column">
+            {{--<div class="ui grid internally celled">--}}
+                <div class="ui four column doubling stackable grid">
+                    @foreach($properties as $property)
+                    <div class="column">
+                        <div class="ui segment special card">
+                            <div class="blurring dimmable image">
+                                <div class="ui dimmer">
                                     <div class="content">
-                                        @if ($property->listingType == 'sale')
-                                            <div class="ui red empty circular label"></div> For Sale
-                                        @elseif($property->listingType == 'rent')
-                                            <div class="ui blue empty circular label"></div> For Rent
-                                        @else
-                                            <div class="ui purple empty circular label"></div> Foreclosure
-                                        @endif
-                                        <div class="meta">
-                                            <span class="date">{{ $property->beds }} bedrooms {{ $property->baths }} baths <br> {{ $property->feet }} sq ft</span>
-                                        </div>
-                                        <div class="description">
-
+                                        <div class="center">
+                                            <h2 class="ui price header">${{ $property->price }}</h2>
+                                            <a href="{{ route('property.single', $property->id) }}">
+                                                <div class="ui inverted button">Show Property</div>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
+                                <img src="{{ asset($property->getFirstImage()) }}" style="height: 250px">
+                                {{--<img src="/images/property/{{ $property->getFirstImage()->file_name  }}">--}}
                             </div>
-                            @endforeach
+                            <div class="content">
+                                @if ($property->listingType == 'sale')
+                                    <div class="ui red empty circular label"></div> For Sale
+                                @elseif($property->listingType == 'rent')
+                                    <div class="ui blue empty circular label"></div> For Rent
+                                @else
+                                    <div class="ui purple empty circular label"></div> Foreclosure
+                                @endif
+                                <div class="meta">
+                                    <span class="date">{{ $property->beds }} bedrooms {{ $property->baths }} baths <br> {{ $property->feet }} sq ft</span>
+                                </div>
+                                <div class="description">
+
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    @endforeach
                 </div>
             </div>
         </div>
